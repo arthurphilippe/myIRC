924673b8267069a30bf343f66a855450bfa78487
Anthony <anthony.tram@epitech.eu>
2018-05-07 15:48:27 +0200

    [CLIENT] Added Errors Functions

---
+++ include/client/client_error.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** Client IRC
+** File description:
+** Errors Headers
+*/
+
+#ifndef CLIENT_ERROR_H_
+	#define CLIENT_ERROR_H_
+
+	#define PRINT_USAGE "USAGE: ./client $host $port"
+	#define MALLOC_FAIL "Memory Allocation Failed"
+	#define ARG2_FAIL ": argument must contain numericals characters"
+	#define WRONG_ARG_NBR "Invalid Arugment"
+
+#endif /* !CLIENT_ERROR_H_ */
