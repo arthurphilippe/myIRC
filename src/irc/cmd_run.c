95c9ea0690b7d0a8fb2dabcf7afc2b1d7fbdaf40
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-05-10 21:03:07 +0200

    finished string to list on single spacers -- got to test with multiple spacers

---
+++ src/irc/cmd_run.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_myirc_2017
+** File description:
+** cmd_run
+*/
+
+#include "irc/cmd.h"
+
+const irc_cmd_t cmd_map[] = {
+	{NULL, NULL},
+};
+
+void irc_cmd_run(manager_t *manager, handle_t *hdl, const char *cmd)
+{
+}
