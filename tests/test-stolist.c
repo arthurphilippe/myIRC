bdf9e4d5f6b55ed928dd33a18d6e14cb6c9b03f3
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-05-10 21:11:50 +0200

    tested and approved for multiple spacers

--- tests/test-stolist.c
+++ tests/test-stolist.c
@@ -50,3 +50,59 @@ Test(stolist, multiple_words) {
 	list_destroy(words);
 	free(iter);
 }
+
+Test(stolist, multiple_words_and_spacers) {
+	list_t *words = stolist("toto va a   la plage avec ses khey et\
+ sa   serviette ; mais sans son code civil. nous,sommes,separes,\
+ par,des ,virgules,", ", ;.");
+
+	cr_assert(words);
+	list_iter_t *iter = list_iter_create(words, FWD);
+	cr_assert(iter);
+	cr_assert_str_eq(list_iter_access(iter), "toto");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "va");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "a");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "la");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "plage");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "avec");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "ses");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "khey");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "et");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "sa");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "serviette");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "mais");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "sans");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "son");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "code");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "civil");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "nous");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "sommes");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "separes");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "par");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "des");
+	list_iter_next(iter);
+	cr_assert_str_eq(list_iter_access(iter), "virgules");
+	list_iter_next(iter);
+	list_destroy(words);
+	free(iter);
+}
