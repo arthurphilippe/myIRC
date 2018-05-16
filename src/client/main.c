71f1712fcafde8d91e3453a2eb6b6f8c07f9ef87
Anthony <anthony.tram@epitech.eu>
2018-05-16 14:19:56 +0200

    [#1] - removed usage

--- src/client/main.c
+++ src/client/main.c
@@ -10,11 +10,6 @@
 #include "handle/server.h"
 #include "client/client.h"
 
-static void print_usage(void)
-{
-	dprintf(2, "%s\n", PRINT_USAGE);
-}
-
 int main(void)
 {
 	manager_t *manager = manager_create_connect();
@@ -22,7 +17,6 @@ int main(void)
 	if (!manager)
 		return (84);
 	manager_loop(manager);
-	print_usage();
 	manager_delete(manager);
 	return (0);
 }
