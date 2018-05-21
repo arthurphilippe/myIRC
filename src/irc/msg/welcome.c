f5df38ef32fee2d0dc5329aa6faa2a950160119d
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-05-21 16:36:07 +0200

    added motd at login

--- src/irc/msg/welcome.c
+++ src/irc/msg/welcome.c
@@ -15,5 +15,11 @@ void irc_msg_welcome(handle_t *hdl)
 
 	dprintf(hdl->h_fd, ":myirc_server 001 %s welcome!\r\n",
 		client->hc_nick);
+	dprintf(hdl->h_fd, ":myirc_server 002 I am your host!\r\n");
+	dprintf(hdl->h_fd,
+		":myirc_server 003 Server was created 05-2018\r\n");
+	dprintf(hdl->h_fd,
+		":myirc_server 004 myirc 1 none none\r\n");
+	dprintf(hdl->h_fd, "376 :End of /MOTD command\r\n");
 	dprintf(hdl->h_fd, ":myirc_server PING :%s\r\n", client->hc_nick);
 }
